5a011d1fcf54c1c05b25a2f99ec8148329538046
Jonathan Blake <jonathan.blake@projectaon.org>
2012-10-28 01:28:34 +0000

    correct page text variable names
    git-svn-id: https://projectaon.org/data/trunk@2153 f6f3e2d7-ff33-0410-aaf5-b4bee2cdac11

--- common/scripts/list-file-sizes.sh
+++ common/scripts/list-file-sizes.sh
@@ -29,25 +29,25 @@ list_dir_size () {
 
 ####
 
-list_file_size xhtml-zip-size \
+list_file_size xhtml_zip_size \
     `find . -path "*xhtml/*$1.zip"`
-list_dir_size xhtml-less-simple-size \
+list_dir_size xhtml_less_simple_size \
     `find . -path "*xhtml-less-simple*$1"`
-list_file_size xhtml-less-simple-zip-size \
+list_file_size xhtml_less_simple_zip_size \
     `find . -path "*xhtml-less-simple/*$1.zip"`
-list_file_size xhtml-simple-size \
+list_file_size xhtml_simple_size \
     `find . -path "*xhtml-simple/*$1.htm"`
-list_file_size xhtml-simple-zip-size \
+list_file_size xhtml_simple_zip_size \
     `find . -path "*xhtml-simple/*$1.zip"`
-list_file_size pdf-size \
+list_file_size pdf_size \
     `find . -name "$1.pdf"`
-list_file_size epub-size \
+list_file_size epub_size \
     `find . -name "$1.epub"`
-list_file_size mobi-size \
+list_file_size mobi_size \
     `find . -name "$1.mobi"`
-list_file_size fb2-size \
+list_file_size fb2_size \
     `find . -name "$1.fb2"`
-list_file_size pdb-size \
+list_file_size pdb_size \
     `find . -name "$1.pdb"`
-list_file_size lrf-size \
-    `find . -name "$1.lrf"`
+list_file_size lrf_size \
+    `find . -name "$1.pdb"`
